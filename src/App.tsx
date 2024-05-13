f76d494ea87ed9d5f880bcbba73108e960f53810
SHRIHASHINI <156648581+SHRIHASHINI@users.noreply.github.com>
2024-05-13 12:33:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Registration from "./Components/Form"
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(1)
 
   return (
     <>
     <Registration></Registration>
-    {/* <registration></registration> */}
-     {/* <registration></registration> */}
     </>
   )
 }
